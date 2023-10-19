e198b158605bf34647832e670103ebd48815ec66
sxmbaka <yatharth.singhhh@gmail.com>
2023-10-19 18:45:08 +0530

    added intitial structure

---
+++ lib/my_calculator_material_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyCalculatorPage extends StatefulWidget {
+  const MyCalculatorPage({super.key});
+
+  @override
+  State<MyCalculatorPage> createState() => _MyCalculatorPageState();
+}
+
+class _MyCalculatorPageState extends State<MyCalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
